b3938eb1f618dfcce8755359f0923fef0d8e9e1f
mateusaraujoramos <mateusaraujoramos.gelt@gmail.com>
2024-11-15 13:27:34 -0300

    refactor: update to currenty program standart

--- ECommerceApp/Program.cs
+++ ECommerceApp/Program.cs
@@ -1,4 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ECommerceApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (connectionString is not null)
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(options => 
+        options.UseSqlServer(connectionString));
+}
+else
+{
+    throw new Exception("Connection String not found");
+}
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
