b043d00ad42d216dc4251f465836d5b33b8f075a
Gustaramos <guram.sil@outlook.com>
2024-12-30 19:26:36 -0300

    creating db mapping

--- ECommerceApp/Entities/BaseEntity.cs
+++ ECommerceApp/Entities/BaseEntity.cs
@@ -1,8 +1,8 @@
 namespace ECommerceApp.Entities;
 
-public class BaseEntity
+public abstract class BaseEntity
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 
