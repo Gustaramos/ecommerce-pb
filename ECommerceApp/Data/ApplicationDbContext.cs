0e0fa31993c308c488eb01de198c4b1ae7749a4f
Gustaramos <guram.sil@outlook.com>
2024-12-30 19:14:11 -0300

    create base entity

--- ECommerceApp/Data/ApplicationDbContext.cs
+++ ECommerceApp/Data/ApplicationDbContext.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
-using ECommerceApp.Models;
+using ECommerceApp.Entities;
+using Product = ECommerceApp.Models.Product;
 
 namespace ECommerceApp.Data
 {
@@ -14,5 +15,35 @@ namespace ECommerceApp.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach(var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                if(entry.State is EntityState.Added)
+                {
+                    entry.Entity.SetCreatedAt(DateTime.UtcNow);
+                }
+
+                if(entry.State is EntityState.Modified)
+                {
+                    entry.Entity.SetUpdatedAt(DateTime.UtcNow);
+                }
+            }
+
+            int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+            return result;
+        }
+
+        public override int SaveChanges()
+        {
+            return SaveChangesAsync().GetAwaiter().GetResult();
+        }
     }
 }
