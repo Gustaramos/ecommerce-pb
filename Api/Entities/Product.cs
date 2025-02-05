a57fe2654b3ee2714df207f987adafb51f7c2f39
BoltzBit <mateus_desktop@outlook.com>
2025-02-04 22:58:11 -0300

    fix: product entity

--- Api/Entities/Product.cs
+++ Api/Entities/Product.cs
@@ -4,7 +4,6 @@ public class Product : BaseEntity
 {
     public string Name { get; set; }
     public string  Description { get; set; }
-    public Guid AttachName { get; set; }
     public decimal Price { get; set; }
     public int Stock { get; set; }
 
@@ -13,13 +12,11 @@ public class Product : BaseEntity
     public Product(
         string name,
         string description,
-        Guid attachName,
         decimal price,
         int stock)
     {
         Name = name;
         Description = description;
-        AttachName = attachName;
         Price = price;
         Stock = stock;
     }
